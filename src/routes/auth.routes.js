25db98e8b6b88567fd3c5b9bf841c08d3f551fc9
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-14 22:18:41 +0530

    userLogoutController

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,3 +9,12 @@ router.post("/register", authController.userRegisterController)
 
 /* POST /api/auth/login */
 router.post("/login",authController.userLoginController)
+
+/**
+ * - POST /api/auth/logout
+ */
+router.post("/logout", authController.userLogoutController)
+
+
+
+module.exports = router
