b297e9c9d36c9eee43f26c1a9786a37d4469d5b5
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-14 22:08:25 +0530

    getUserAccountsController

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -13,3 +13,10 @@ const router = express.Router()
  * - Protected Route
  */
 router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
+
+/**
+ * - GET /api/accounts/
+ * - Get all accounts of the logged-in user
+ * - Protected Route
+ */
+router.get("/", authMiddleware.authMiddleware, accountController.getUserAccountsController)
