50741498d64203857ae0da91952b19cb1cf1e893
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-14 21:52:11 +0530

    tokenBlackListMol added

--- src/models/blackList.model.js
+++ src/models/blackList.model.js
@@ -10,3 +10,11 @@ const tokenBlacklistSchema = new mongoose.Schema({
 }, {
     timestamps: true
 })
+
+tokenBlacklistSchema.index({ createdAt: 1 }, {
+    expireAfterSeconds: 60 * 60 * 24 * 3 // 3 days
+})
+
+const tokenBlackListModel = mongoose.model("tokenBlackList", tokenBlacklistSchema);
+
+module.exports = tokenBlackListModel;
