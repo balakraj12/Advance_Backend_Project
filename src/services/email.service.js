862a45f261f984177fa122667eb7833c68e5fffa
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-15 14:21:49 +0530

    Function to send email

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -19,3 +19,21 @@ transporter.verify((error, success) => {
         console.log('Email server is ready to send messages');
     }
 });
+
+// Function to send email
+const sendEmail = async (to, subject, text, html) => {
+    try {
+        const info = await transporter.sendMail({
+            from: `"Backend Ledger" <${process.env.EMAIL_USER}>`, // sender address
+            to, // list of receivers
+            subject, // Subject line
+            text, // plain text body
+            html, // html body
+        });
+
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+};
