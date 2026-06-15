4167a999c065d6fa77abc921e72c8b7a7fdefd22
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-15 14:36:07 +0530

    Use Routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const cookieParser = require("cookie-parser")
 
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
+
 app.use(express.json());
+app.use(cookieParser());
+
+/**
+ * - Routes required
+ */
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
+
+/**
+ * - Use Routes
+ */
+
 
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes)
+app.use("/api/accounts", accountRouter)
+app.use("/api/transactions", transactionRoutes)
 
 module.exports = app;
